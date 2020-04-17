3664d7dfa5cd28bf80a960c2c1188a8047ebdae1
Kamran <kamranasif520@gmail.com>
2020-04-17 10:02:35 +0500

    Updates

---
+++ precache-manifest.89d6869451dd5801d5cc5d5d77b6faff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb884ac8290ef63f94e14ebf2f8cbf3",
+    "url": "/drawertest/index.html"
+  },
+  {
+    "revision": "92723a083fdc4be423b4",
+    "url": "/drawertest/static/css/main.bf92f223.chunk.css"
+  },
+  {
+    "revision": "8d27b25ca25b97d7635d",
+    "url": "/drawertest/static/js/2.855f72ea.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drawertest/static/js/2.855f72ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92723a083fdc4be423b4",
+    "url": "/drawertest/static/js/main.05a3a06d.chunk.js"
+  },
+  {
+    "revision": "d6317fb5745f96a510c0",
+    "url": "/drawertest/static/js/runtime-main.9401f422.js"
+  }
+]);
